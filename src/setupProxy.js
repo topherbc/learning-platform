f44357c8440b8e88db7be8027c9796204e131d0e
Topher <topherbyron@duck.com>
2025-01-21 21:19:52 -0500

    fix: update proxy configuration for correct port routing

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,6 +6,15 @@ module.exports = function(app) {
     createProxyMiddleware({
       target: 'http://localhost:3001',
       changeOrigin: true,
+      secure: false,
+      logLevel: 'debug',
+      onError: (err, req, res) => {
+        console.error('Proxy Error:', err);
+        res.status(500).send('Proxy Error');
+      },
+      pathRewrite: {
+        '^/api': '/api'
+      }
     })
   );
 };
